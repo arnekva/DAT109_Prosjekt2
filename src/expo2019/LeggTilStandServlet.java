b0f6ccb6ac8d8b16c91cd372366645df803c4482
Arne <arnekva@gmail.com>
2019-03-12 12:21:19 +0100

    Tiny commit

--- src/expo2019/LeggTilStandServlet.java
+++ src/expo2019/LeggTilStandServlet.java
@@ -29,7 +29,7 @@ public class LeggTilStandServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/logginn.jsp").forward(request, response);
 	}
 
 	/**
