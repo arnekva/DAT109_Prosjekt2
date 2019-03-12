d93ef8d5241a7f98c2a073a6388efd66d9843a67
Marcus Korsnes Morlandstø <marcum16@student.uia.no>
2019-03-12 13:40:24 +0100

    Skrevet loggInn og loggInnServlet, ikke komplett

--- src/expo2019/LoggInn.java
+++ src/expo2019/LoggInn.java
@@ -1,5 +1,66 @@
 package expo2019;
 
+import javax.servlet.http.HttpServletRequest;
+
+
 public class LoggInn {
 
+	private String mobilnr;
+	private String passord;
+	private String feilmelding;
+	
+	private String tillatmobilnr = "^[0-9]+$";
+	private String tillatpassord = "^[a-zæøåA-ZÆØÅ0-9 .]+$";
+	
+	private StandEAO standEAO;
+	
+	public LoggInn(HttpServletRequest request, StandEAO standEAO) {
+		this.mobilnr = (String) request.getParameter("mobil");
+		this.passord = (String) request.getParameter("passord");
+		this.feilmelding = "";
+		this.standEAO = standEAO;
+	}
+	
+	public boolean isInputValid() {
+		return this.mobilnr.matches(tillatmobilnr) && this.passord.matches(tillatpassord);
+	}
+	
+	public void genererFeilmelding() {
+		if (!isInputValid() || !isLogginnValid()) {
+			mobilnr = "";
+			passord = "";
+			feilmelding = "Ugyldig telefonummer og/eller passord!";
+		}
+	}
+	
+	public boolean isLogginnValid() {
+		try {
+			if (isInputValid()) {
+				User user = hentUser();
+				//Rediger return statement når krypterings metodene er implementert
+				return true;
+			} else {
+				return false;
+			} 
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public String getMobilnr() {
+		return mobilnr;
+	}
+
+	public String getPassord() {
+		return passord;
+	}
+
+	public String getFeilmelding() {
+		return feilmelding;
+	}
+	
+	public User hentUser() {
+		User user = standEAO.hentBrukerPaaPK(Integer.parseInt(this.mobilnr));
+		return user;
+	}
 }
