efe195c0ee77fb14afffc51263fac8807863471a
Arne <arnekva@gmail.com>
2019-03-13 17:07:17 +0100

    Updated jsp

--- src/expo2019/StandListeServlet.java
+++ src/expo2019/StandListeServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 /**
@@ -25,7 +26,7 @@ public class StandListeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		HttpSession sesjon = request.getSession(false);
 		List<Stand> standliste = standEAO.hentAlleStands();
 		
 		request.getSession().setAttribute("standliste", standliste);
