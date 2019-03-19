25c13c4bf53c4b12ff3b89490180328f8b864ff6
571734 <571734@stud.hvl.no>
2019-03-19 14:05:21 +0100

    Lagt til testklasse

---
+++ src/test/DatabaseTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import javax.ejb.EJB;
+
+import org.junit.Test;
+
+import expo2019.StandEAO;
+import expo2019.StandRating;
+
+public class DatabaseTest {
+	
+	@EJB
+	private StandEAO standEAO;
+
+	@Test
+	public void hentRatingTest() {
+		StandRating sr =
+	}
+
+}
