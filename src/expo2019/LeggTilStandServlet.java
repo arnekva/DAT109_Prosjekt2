cdff2c71027e9cc5095b9582de6b2946bc88d2a6
571734 <571734@stud.hvl.no>
2019-04-02 12:16:25 +0200

    La til https://

--- src/expo2019/LeggTilStandServlet.java
+++ src/expo2019/LeggTilStandServlet.java
@@ -54,7 +54,7 @@ public class LeggTilStandServlet extends HttpServlet {
 			System.out.println(id);
 			//TODO: Fiks link
 			String qr = "https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=" 
-			+ "data1.hib.no:9090/expo2019_gruppe7/stand" + "?standid=" + id;
+			+ "http://data1.hib.no:9090/expo2019_gruppe7/stand" + "?standid=" + id;
 			stand.setQrUrl(qr);
 			standEAO.leggTilStand(stand);
 			response.sendRedirect("stands");
